3fbf83a8aa6d734641c406c33fe3575201bbbc8d
sfahadahmed <sfahadahmed@gmail.com>
2018-02-23 13:11:55 +0500

    Adding initial support for adding a new Todo item.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { TodoListComponent } from './components/todo-list/todo-list.component';
 
-import { TodoService } from './services/todo.service'
+import { TodoService } from './services/todo.service';
+import { TodoAddComponent } from './components/todo-add/todo-add.component'
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoListComponent
+    TodoListComponent,
+    TodoAddComponent
   ],
   imports: [
     BrowserModule,
