08f1e2f223fbff890d3812df4b9981e9b0ae5bf3
sfahadahmed <sfahadahmed@gmail.com>
2018-02-23 13:09:47 +0500

    Adding complete support for CRUD operations for the REST api.

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -5,10 +5,27 @@ import { Observable } from 'rxjs/Observable';
 @Injectable()
 export class TodoService {
 
+  baseURL: string = 'http://localhost:8080/api/todoitems';
+
   constructor(private http: HttpClient) { }
   
     getAll(): Observable<any> {
-      return this.http.get('http://localhost:8080/api/todoitems');
+      return this.http.get(this.baseURL);
     }
 
+    get(id: number): Observable<any> {
+      return this.http.get(this.baseURL+'/'+id);
+    }
+
+    create(todoItem: any): Observable<any> {
+      return this.http.post(this.baseURL, todoItem);
+    }
+
+    update(id: number, todoItem: any): Observable<any> {
+      return this.http.put(this.baseURL+'/'+id, todoItem);
+    }
+
+    delete(id: number): Observable<any> {
+      return this.http.delete(this.baseURL+'/'+id);
+    }
 }
