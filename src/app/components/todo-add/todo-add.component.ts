cff905dcce4037d0bd46503ef3564bc44a4328f5
sfahadahmed <sfahadahmed@gmail.com>
2018-02-23 18:04:38 +0500

    Updating the UI - Support for two-way data binding - Adding new checkbox for the "active" field - Changing "description" field to text area - Adding cancel button - Adding location-based routing support to go back and forth using the Angular's routing navigation

--- src/app/components/todo-add/todo-add.component.ts
+++ src/app/components/todo-add/todo-add.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { TodoItem } from '../../todo-item';
+import { TodoService } from '../../services/todo.service';
 
 @Component({
   selector: 'app-todo-add',
   templateUrl: './todo-add.component.html',
-  styleUrls: ['./todo-add.component.css']
+  styleUrls: ['./todo-add.component.css'],
 })
 export class TodoAddComponent implements OnInit {
 
-  constructor() { }
+  @Input() todoItem: TodoItem;
+
+  constructor(private todoService: TodoService,
+    private route: ActivatedRoute,
+    private location: Location) { }
 
   ngOnInit() {
+    this.todoItem = new TodoItem();
   }
 
+  addTodoItem(): void {
+    console.log("-- addTodoItem() --");
+    console.log(this.todoItem);
+    this.todoService.create(this.todoItem).subscribe(response => {
+      this.location.back();
+    });
+  }
+
+  cancel(): void {
+    this.location.back();
+  }
 }
---
+++ src/app/todo-item.ts
@@ -0,0 +1,5 @@
+export class TodoItem {
+    title: string;
+    description: string;
+    active: boolean;
+}
