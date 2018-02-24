e6d36c38a9e2a3bd06ff3592cebbc62d4b3a046f
sfahadahmed <sfahadahmed@gmail.com>
2018-02-24 11:06:36 +0500

    Updating the routing module with the route for the view/edit screen component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { RouterModule, Route } from '@angular/router';
 
 import { TodoAddComponent } from './components/todo-add/todo-add.component';
 import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { TodoDetailComponent } from './components/todo-detail/todo-detail.component';
 
 const routes: Route[] = [
   {path: '', redirectTo: '/list', pathMatch: 'full'},
   {path: 'list', component: TodoListComponent},
-  {path: 'add', component: TodoAddComponent}
+  {path: 'add', component: TodoAddComponent},
+  {path: 'detail/:id', component: TodoDetailComponent}
 ];
 
 @NgModule({
